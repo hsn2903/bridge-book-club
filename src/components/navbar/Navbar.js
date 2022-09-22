348614d2646375e37002e86d666bc8ead535e395
Hasan Çelik <hsn.2903@outlook.com>
2022-09-22 08:31:42 +0300

    navbar updated

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,25 +1,28 @@
 import React, { useState, useRef, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
-import { links, social } from "./data";
+
 import "./Navbar.css";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
-  const linksContainerRef = useRef(null);
-  const linksRef = useRef(null);
+  // const linksContainerRef = useRef(null);
+  // const linksRef = useRef(null);
+
   const toggleLinks = () => {
     setShowLinks(!showLinks);
   };
-  useEffect(() => {
-    const linksHeight = linksRef.current.getBoundingClientRect().height;
-    if (showLinks) {
-      linksContainerRef.current.style.height = `${linksHeight}px`;
-    } else {
-      linksContainerRef.current.style.height = "0px";
-    }
-  }, [showLinks]);
+
+  // useEffect(() => {
+  //   const linksHeight = linksRef.current.getBoundingClientRect().height;
+  //   if (showLinks) {
+  //     linksContainerRef.current.style.height = `${linksHeight}px`;
+  //   } else {
+  //     linksContainerRef.current.style.height = "0px";
+  //   }
+  // }, [showLinks]);
+
   return (
-    <div className="section-nav">
+    <div className="nav_section">
       <nav>
         <div className="nav-center">
           <div className="nav-header">
@@ -31,8 +34,12 @@ const Navbar = () => {
               <FaBars />
             </button>
           </div>
-          <div className="links-container" ref={linksContainerRef}>
-            <ul className="links" ref={linksRef}>
+          <div
+            className={`${
+              showLinks ? "links-container show-container" : "links-container"
+            }`}
+          >
+            <ul className="links">
               <li>
                 <a href="">Home</a>
               </li>
