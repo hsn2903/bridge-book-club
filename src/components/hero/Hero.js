b3fc4527e6f399015aec4e090e849630f733b571
Hasan Çelik <hsn.2903@outlook.com>
2022-09-27 19:36:36 +0300

    update hero

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,29 +3,27 @@ import heroImg from "../../assets/img/hero-img.png";
 
 const Hero = () => {
   return (
-    <section className="hero_section layout_padding">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <div className="detail-box">
-              <h5>Bridge Book Club</h5>
-              <h1>
-                Meeting Station <br />
-                For Book Lovers
-              </h1>
-              <p>
-                We are meeting <strong>on Saturday at 20:00 UTC+3</strong> for
-                discussing books. If you want to want more informationi please
-                contact with us.
-              </p>
-              <a href="#contact_section"> Contact Us</a>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="img-box">
-              <img src={heroImg} alt="people reading books" />
-            </div>
-          </div>
+    <section className="section_hero">
+      <div className="hero">
+        <div className="hero-text-box">
+          <span className="subheading">Bridge Book Club</span>
+          <h1 className="heading-primary">
+            Book Club <br />
+            For English Learners
+          </h1>
+          <p className="hero-description">
+            We meet <strong>on Saturday at 20:00 UTC+3</strong> for discussing
+            books. If you want to want more information please contact with us.
+          </p>
+          <a className="btn btn__full margin-right-sm" href="#">
+            Contact us
+          </a>
+          <a className="btn btn__outline" href="#">
+            Learn more &darr;
+          </a>
+        </div>
+        <div className="hero-img-box">
+          <img src={heroImg} alt="people reading books" className="hero-img" />
         </div>
       </div>
     </section>
