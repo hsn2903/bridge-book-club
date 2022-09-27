2f7c46f676119ede8f2dbdd40882403e6a1148a7
Hasan Çelik <hsn.2903@outlook.com>
2022-09-27 19:33:37 +0300

    update navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -12,58 +12,83 @@ const Navbar = () => {
   };
 
   return (
-    <div className="nav_section">
-      <nav>
-        <div className="container nav__container">
-          <Link to="/" className="nav__brand">
-            <span> Bridge Book Club </span>
-          </Link>
+    <header className="section_nav">
+      <Link to="/" className="logo">
+        <span> Bridge Book Club </span>
+      </Link>
 
-          <ul className={`nav__links ${showLinks ? "show__nav" : "hide__nav"}`}>
-            <li>
-              <NavLink
-                to="home"
-                className={({ isActive }) => (isActive ? "active-nav" : "")}
-                onClick={toggleLinks}
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="about"
-                className={({ isActive }) => (isActive ? "active-nav" : "")}
-                onClick={toggleLinks}
-              >
-                About
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="blog"
-                className={({ isActive }) => (isActive ? "active-nav" : "")}
-                onClick={toggleLinks}
-              >
-                Blog
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="purpose"
-                className={({ isActive }) => (isActive ? "active-nav" : "")}
-                onClick={toggleLinks}
-              >
-                Purpose & Rules
-              </NavLink>
-            </li>
-          </ul>
+      <nav className="main-nav">
+        <ul
+          className={`main-nav-list ${showLinks ? "show__nav" : "hide__nav"}`}
+        >
+          <li>
+            <a href="#how" className="main-nav-link">
+              How it works
+            </a>
+            {/* <NavLink
+              to="#how"
+              className={({ isActive }) =>
+                isActive ? "main-nav-link active-nav" : "main-nav-link"
+              }
+              onClick={toggleLinks}
+            >
+              How it works
+            </NavLink> */}
+          </li>
+          <li>
+            <NavLink
+              to="about"
+              className={({ isActive }) =>
+                isActive ? "main-nav-link active-nav" : "main-nav-link"
+              }
+              onClick={toggleLinks}
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="blog"
+              className={({ isActive }) =>
+                isActive ? "main-nav-link active-nav" : "main-nav-link"
+              }
+              onClick={toggleLinks}
+            >
+              Blog
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="purpose"
+              className={({ isActive }) =>
+                isActive ? "main-nav-link active-nav" : "main-nav-link"
+              }
+              onClick={toggleLinks}
+            >
+              Purpose & Rules
+            </NavLink>
+          </li>
 
-          <button className="nav__toggle-btn" onClick={toggleLinks}>
-            {!showLinks ? <FaBars /> : <FaTimes />}
-          </button>
-        </div>
+          <li>
+            <NavLink
+              to="contact"
+              className={({ isActive }) =>
+                isActive
+                  ? "main-nav-link nav-cta active-nav"
+                  : "main-nav-link nav-cta"
+              }
+              onClick={toggleLinks}
+            >
+              Purpose & Rules
+            </NavLink>
+          </li>
+        </ul>
+
+        <button className="nav__toggle-btn" onClick={toggleLinks}>
+          {!showLinks ? <FaBars /> : <FaTimes />}
+        </button>
       </nav>
-    </div>
+    </header>
   );
 };
 
